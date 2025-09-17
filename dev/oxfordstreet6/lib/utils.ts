3f80c007ff67208b6a3a41ed7d07ad008dd28efe
housemedical <housemedicalconsultants@gmail.com>
2025-09-18 00:07:44 +0100

    Delete dev/oxfordstreet6 directory

--- dev/oxfordstreet6/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-export function cx(...classes: Array<string | false | undefined | null>) {
-  return classes.filter(Boolean).join(' ');
-}
-
-/** Ensures external links use security attributes. */
-export function externalProps() {
-  return { target: "_blank", rel: "noopener noreferrer" };
-}
